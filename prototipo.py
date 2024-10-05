7b098f1c9b7f5fae93b1fd4eb08616433e1c8704
SamuellRock <samuell.rocha.01@gmail.com>
2024-10-04 23:03:03 -0300

    first commit

---
+++ prototipo.py
@@ -0,0 +1,32 @@
+import os
+from dotenv import load_dotenv
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_groq import ChatGroq
+from langchain_core.output_parsers import StrOutputParser
+
+# Carrega as variáveis do arquivo .env
+load_dotenv()
+
+# Função para pegar a resposta do LLM e configura o cliente Groq
+async def get_response(user_input):
+    llm = ChatGroq(
+        model="mixtral-8x7b-32768",
+        temperature=0,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        api_key=os.environ.get("api"),  #congirurar no .env com a Chave API
+    )
+
+    messages = [
+        SystemMessage(content="Você é um assistente que irá responder tudo em português."),
+        HumanMessage(content=user_input)
+    ]
+
+    parser = StrOutputParser()
+
+    chain = llm | parser
+
+    texto = chain.invoke(messages)
+
+    return texto
---
+++ servidor.py
@@ -0,0 +1,33 @@
+import os
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+from prototipo import get_response
+load_dotenv()
+
+
+app = FastAPI()
+
+# Configurar CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/chat")
+async def chat(message: dict):
+    user_message = message.get('message')
+    if not user_message:
+        return JSONResponse(content={"error": "Mensagem não fornecida"}, status_code=400)
+
+    response_text = await get_response(user_message)
+    return JSONResponse(content={"response": response_text})
+
+# Executa o servidor
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
